8d59d71fd36bf32ce31ae498c677c2b99b1b4967
Sean Flaherty <sean.flaherty@gmail.com>
2025-04-19 17:04:46 -0400

    Add pytest mark decorators.

--- fizzbuzz_tests/test_fizzbuzz.py
+++ fizzbuzz_tests/test_fizzbuzz.py
@@ -1,16 +1,20 @@
 # imports
+import pytest
 from ..src.fizzbuzz import divide_by_three, divide_by_five, divide_by_three_and_five
 
+@pytest.mark.regression
 # 	1. If the integer is divisible by 3, return the string "Fizz."
 def test_divide_by_three():
     assert divide_by_three(9) == "Fizz", "Expected 9 should return Fizz."
     assert divide_by_three(8) == "", "Expected empty string."
 
+@pytest.mark.regression
 # 	2. If the integer is divisible by 5, return the string "Buzz."
 def test_divide_by_five():
     assert divide_by_five(15) == "Buzz", "Expected 15 should return Buzz."
     assert divide_by_five(8) == "", "Expected empty string."
 
+@pytest.mark.regression
 # 	3. If the integer is divisible by 3 and 5, return the string "FizzBuzz."
 def test_divide_by_three_and_five():
     assert divide_by_three_and_five(15) == "FizzBuzz", "Expected 15 should return FizzBuzz."
